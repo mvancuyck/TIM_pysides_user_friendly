6212d06cdeb8f761a17281b31b339aef18148795
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2024-07-05 15:42:45 -0500

    update

--- script.sh
+++ script.sh
@@ -8,7 +8,4 @@
 #chmod +x me!
 python gen_all_size_cat.py 
 python gen_all_TIM_cubes.py 
-python plot_tim_maps
-python variance_0.1deg2.py --recompute
-python variance_1deg2.py --recompute
-python cross_spec_snr_in_tim.py 'outputs_uchuu/'
+python compute_all_p_of_k
