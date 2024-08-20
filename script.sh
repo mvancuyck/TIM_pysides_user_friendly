5136eb46a6ab7ff97d544b2b519c3a940780d66c
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2024-08-20 12:24:52 -0500

    update

--- script.sh
+++ script.sh
@@ -6,6 +6,7 @@
 #module load mpi/openmpi/4.0.1
 
 #chmod +x me!
-#python gen_all_sizes_cat.py 
-python gen_all_sizes_TIM_cubes.py 
-python compute_all_p_of_k.py
+python gen_all_sizes_cat.py 
+python gen_all_sizes_TIM_angular_spectral_cubes.py
+#python gen_all_sizes_TIM_cubes.py 
+#python compute_all_p_of_k.py
