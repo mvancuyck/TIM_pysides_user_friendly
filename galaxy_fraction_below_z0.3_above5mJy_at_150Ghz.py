fcb8dbd236e5ed0519270355f4e8d62553dc9cba
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2024-10-23 11:49:24 -0500

    add new project

--- galaxy_fraction_below_z0.3_above5mJy_at_150Ghz.py
+++ galaxy_fraction_below_z0.3_above5mJy_at_150Ghz.py
@@ -11,6 +11,8 @@ from matplotlib.colors import LogNorm
 import matplotlib.patches as patches
 import re
 import glob
+from progress.bar import Bar
+
 
 freq_CII = 1900.53690000 * u.GHz
 
@@ -44,30 +46,50 @@ def sorted_files_by_n(directory, tile_sizes):
 if __name__ == "__main__":
 
     params_sides = load_params('PAR_FILES/SIDES_from_original_with_fir_lines.par')
-    TIM_params = load_params('PAR_FILES/Uchuu_cubes_for_TIM.par')
+    TIM_params   = load_params('PAR_FILES/Uchuu_cubes_for_TIM.par')
 
     for tile_sizeRA, tile_sizeDEC in TIM_params['tile_sizes']: 
         
         # List files matching the pattern
         files = sorted_files_by_n(TIM_params["sides_cat_path"], ((tile_sizeRA, tile_sizeDEC),))
+
+        list_fract = np.zeros(len(files))
+
+        dict_name = f'galaxy_fraction_below_z.3_above5mJy_at_150GHz_in_{tile_sizeRA}degx{tile_sizeDEC}deg.p'
+        if(not os.path.isfile(dict_name)):
+
+            dict = {}
+            bar = Bar(f'Processing {tile_sizeRA}degx{tile_sizeDEC}deg', max=len(files))
+
+            for l, file in enumerate(files):
+
+                dict[f'{l}'] = {}
+                
+                cat = Table.read(TIM_params["sides_cat_path"]+file)
+                cat = cat.to_pandas()
+                cat = cat.loc[cat['redshift']<=0.3]
+
+                channels = (150e9,)
+                treshold = 5e-3 #Jy 
+                lambda_list =  ( cst.c * (u.m/u.s)  / (np.asarray(channels)* u.Hz)  ).to(u.um)
+                SED_dict = pickle.load(open(params_sides['SED_file'], "rb"))    
+                Snu_arr = gen_Snu_arr(lambda_list.value, 
+                                    SED_dict, cat["redshift"],
+                                    cat["LIR"], cat["Umean"], 
+                                    cat["Dlum"], cat["issb"])
+                
+                dict[f'{l}']['Snu_arr'] = Snu_arr
+                dict[f'{l}']['len_cat_z<=0.3'] = len(cat)
+                a = np.where(Snu_arr[:,0].value>treshold)
+                dict[f'{l}']['nb_sources_above_treshold'] = len(a[0])
+
+                list_fract[l] = len(a[0]) / len(cat)
+
+                bar.next()
+            dict['mean_frac'] = list_fract.mean()
+            dict['std_frac'] = list_fract.std()
+            bar.finish
+            pickle.dump(dict, open(dict_name, 'wb'))
         
-        for l, file in enumerate(files):
-            
-            cat = Table.read(TIM_params["sides_cat_path"]+file)
-            embed()
-            cat = cat.loc[cat['redshift']<=0.3]
-            cat = cat.to_pandas()
-    
-            channels = (150e9,)
-            lambda_list =  ( cst.c * (u.m/u.s)  / (np.asarray(channels)* u.Hz)  ).to(u.um)
-            SED_dict = pickle.load(open(params_sides['SED_file'], "rb"))    
-            print("Generate continuum fluxes...")
-            Snu_arr = gen_Snu_arr(lambda_list.value, 
-                                  SED_dict, cat["redshift"],
-                                  cat["LIR"], cat["Umean"], 
-                                  cat["Dlum"], cat["issb"])
-            
-
-            embed()
-
+        else: dict =  pickle.load( open(dict_name, 'rb'))
 
