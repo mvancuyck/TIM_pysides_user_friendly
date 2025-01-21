d63177e552654d1189aa4ab83808308633a4fe95
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-01-20 18:56:38 -0600

    update

--- script.sh
+++ script.sh
@@ -1,12 +1,20 @@
-#!/bin/bash
-#SBATCH --ntasks-per-node=24
-#SBATCH --nodes=5
-#SBATCH --cpus-per-task=1
-#SBATCH -p batch
-#module load mpi/openmpi/4.0.1
-
 #chmod +x me!
+
+#-- I generate all the sub-catalogues for the field size you specified in the .par, from the 117deg2 Uchuu catalogue.
+#-- But you can also download them elsewhere. 
 python gen_all_sizes_cat.py 
+#-- From these subcatalogues, I generate the angular spectral cubes in the TIM bandpass.
 python gen_all_sizes_TIM_angular_spectral_cubes.py
-#python gen_all_sizes_TIM_cubes.py 
-#python compute_all_p_of_k.py
+
+#-- Once you generated some angular spectral cubes, I can generate the gif
+python all_gif_maker.py
+
+
+#-- And I generate physical (i.e Mpc) CII cubes as covered by SIDES, and compute their 3D power spectrum
+python gen_all_sizes_TIM_cubes.py 
+python compute_all_p_of_k.py
+#Then plot the results of the measured 3d power spectra with pk_CII_result.ipynb
+#--- With pks_sanitycheck.py you can look at each individual 3d power spectra computed in compute_all_p_of_j.py
+python pks_sanitycheck.py
+#--- the kmodes_in_rectangle_cubes.ipynb explores which k modes in Mpc-1 units are available in 
+#--- the 4 redshift bins of a typicall TIM CII cubes. 
