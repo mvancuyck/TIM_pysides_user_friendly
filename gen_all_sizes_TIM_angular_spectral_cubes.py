e2f93d63501323a7056440fb06ee24dd09f31dd2
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-01-23 09:16:50 -0600

    update

--- gen_all_sizes_TIM_angular_spectral_cubes.py
+++ gen_all_sizes_TIM_angular_spectral_cubes.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
                 TIM_params = load_params(f'PAR_FILES/Uchuu_minicubes_for_TIM_{zrange}.par')
 
                 #Generate the TIM cubes with params precised in TIM_params.par
-                TIM_params['run_name'] = f"pySIDES_from_uchuu_TIM_tile{l}_{zrange}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz"
+                TIM_params['run_name'] = f"pySIDES_from_uchuu_TIM_tile{l}_{zrange}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz_"
                 file = TIM_params['output_path'] +  TIM_params['run_name'] + 'all_lines_de_Looze_smoothed_MJy_sr.fits' 
 
                 if(not os.path.isfile(file)): 
