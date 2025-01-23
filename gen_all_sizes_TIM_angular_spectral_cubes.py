ca836610e6f16a07b3fa798c1b51594d8341a179
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-01-23 11:01:06 -0600

    update

--- gen_all_sizes_TIM_angular_spectral_cubes.py
+++ gen_all_sizes_TIM_angular_spectral_cubes.py
@@ -80,21 +80,17 @@ if __name__ == "__main__":
 
             for zrange in ('highz', 'lowz', 'midz'):
 
+                #Load the catalog of the subfield
+                cat = Table.read(TIM_params["sides_cat_path"]+cfile)
+                cat = cat.to_pandas()
+
                 TIM_params = load_params(f'PAR_FILES/Uchuu_minicubes_for_TIM_{zrange}.par')
 
                 #Generate the TIM cubes with params precised in TIM_params.par
                 TIM_params['run_name'] = f"pySIDES_from_uchuu_TIM_tile{l}_{zrange}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz_"
                 file = TIM_params['output_path'] +  TIM_params['run_name'] + '_all_lines_de_Looze_smoothed_MJy_sr.fits' 
+                if(not os.path.isfile(file)): make_cube(cat, params_sides, TIM_params)
 
-                #Load the catalog of the subfield
-                cat = Table.read(TIM_params["sides_cat_path"]+cfile)
-                cat = cat.to_pandas()
-                
-                if(not os.path.isfile(file)): 
-
-                    make_cube(cat, params_sides, TIM_params)
-
-
-                TIM_params['run_name'] = f"pySIDES_from_uchuu_guassian_TIM_tile{l}_{zrange}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz_"
-                file = TIM_params['output_path'] +  TIM_params['run_name'] + 'all_lines_de_Looze_smoothed_MJy_sr.fits' 
-                make_cube(cat, params_sides, TIM_params, filter=True)
+                TIM_params['run_name'] = f"pySIDES_from_uchuu_guassian_TIM_tile{l}_{zrange}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz"
+                file = TIM_params['output_path'] +  TIM_params['run_name'] + '_all_lines_de_Looze_smoothed_MJy_sr.fits' 
+                if(not os.path.isfile(file)): make_cube(cat, params_sides, TIM_params, filter=True)
