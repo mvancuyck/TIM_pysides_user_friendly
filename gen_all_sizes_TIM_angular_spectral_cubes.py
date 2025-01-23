cfe773e7d5d77a83ac326dad24a311885763e77a
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-01-23 10:16:08 -0600

    update

--- gen_all_sizes_TIM_angular_spectral_cubes.py
+++ gen_all_sizes_TIM_angular_spectral_cubes.py
@@ -92,3 +92,8 @@ if __name__ == "__main__":
                     cat = Table.read(TIM_params["sides_cat_path"]+cfile)
                     cat = cat.to_pandas()
                     make_cube(cat, params_sides, TIM_params)
+
+
+                TIM_params['run_name'] = f"pySIDES_from_uchuu_guassian_TIM_tile{l}_{zrange}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz_"
+                file = TIM_params['output_path'] +  TIM_params['run_name'] + 'all_lines_de_Looze_smoothed_MJy_sr.fits' 
+                make_cube(cat, params_sides, TIM_params, filter=True)
