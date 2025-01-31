f075209ffe460b344e31cf9529ee46682a0dfaae
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-01-31 09:58:49 -0600

    update

--- pysides/make_cube.py
+++ pysides/make_cube.py
@@ -253,9 +253,7 @@ def channel_flux_densities(cat, params_sides, cube_prop_dict, params, filter=Fal
         lambda_lower_bound = ( cst.c * (u.m/u.s)  / (np.asarray(lower_bounds) * u.Hz)  ).to(u.um)
         lambda_upper_bound = ( cst.c * (u.m/u.s)  / (np.asarray(upper_bounds) * u.Hz)  ).to(u.um)
         
-        Snu_arr, nu_obs_sed_Hz, channels_list = gen_Snu_arr_filter(lambda_list.value, 
-                                                                     lambda_lower_bound.value,  
-                                                                     lambda_upper_bound.value, 
+        Snu_arr, sed_Jy, nu_obs_Hz = gen_Snu_arr_filter((lambda_lower_bound.min(), lambda_upper_bound.max()), 
                                                                      SED_dict, cat["redshift"], 
                                                                      cat['mu']*cat["LIR"], cat["Umean"], 
                                                                      cat["Dlum"], cat["issb"])
