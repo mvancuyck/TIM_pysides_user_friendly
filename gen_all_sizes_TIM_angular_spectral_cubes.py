195bb39b8e18db59d46789805d928fc1268b171a
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-02-06 14:13:16 -0600

    update

--- gen_all_sizes_TIM_angular_spectral_cubes.py
+++ gen_all_sizes_TIM_angular_spectral_cubes.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     params_sides = load_params('PAR_FILES/SIDES_from_original_with_fir_lines.par')
     TIM_params = load_params('PAR_FILES/Uchuu_cubes_for_TIM.par')
     CONCERTO_params = load_params('PAR_FILES/Uchuu_cubes_for_CONCERTO.par')
-    """
+    
     for i, (tile_sizeRA, tile_sizeDEC) in enumerate(TIM_params['tile_size']): 
         
         # List files matching the pattern
@@ -57,7 +57,7 @@ if __name__ == "__main__":
             TIM_params['run_name'] = f"pySIDES_from_uchuu_TIM_tile{l}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz"
             file = TIM_params['output_path'] +  TIM_params['run_name'] + '_full_de_Looze_smoothed_MJy_sr.fits' 
 
-            if(not os.path.isfile(file)):
+            if(not os.path.isfile(file) and False):
 
                 #Load the catalog of the subfield
                 cat = Table.read(TIM_params["sides_cat_path"]+cfile)
@@ -65,8 +65,11 @@ if __name__ == "__main__":
             
                 make_cube(cat, params_sides, TIM_params)
 
+            #To uncomment later for the gif.
+            #-------------------------------
+            """
             TIM_params['run_name'] = f"pySIDES_from_uchuu_gaussian_TIM_tile{l}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz"
-            file = TIM_params['output_path'] +  TIM_params['run_name'] + '_all_lines_de_Looze_smoothed_MJy_sr.fits' 
+            file = TIM_params['output_path'] +  TIM_params['run_name'] + '_full_de_Looze_smoothed_MJy_sr.fits' 
             if(not os.path.isfile(file) and l==0):
 
                 TIM_params['save_continuum_only'] = False
@@ -78,6 +81,8 @@ if __name__ == "__main__":
             
                 make_cube(cat, params_sides, TIM_params, filter=True)
                 TIM_params = load_params('PAR_FILES/Uchuu_cubes_for_TIM.par')
+            """
+            #-------------------------------
 
 
             #Generate the CONCERTO cubes if wanted
@@ -86,11 +91,9 @@ if __name__ == "__main__":
             file = CONCERTO_params['output_path'] +  CONCERTO_params['run_name'] + 'full_de_Looze_smoothed_MJy_sr.fits' 
             if(not os.path.isfile(file)): make_cube(cat, params_sides, CONCERTO_params)
             '''
-    """
+    
     #generate smaller chunks of the TIM cube
     for tile_sizeRA, tile_sizeDEC in TIM_params['tile_size']: 
-
-        if(tile_sizeRA == 1.414): continue
 
         # List files matching the pattern
         files = sorted_files_by_n(TIM_params["sides_cat_path"], ((tile_sizeRA, tile_sizeDEC),))
@@ -101,15 +104,16 @@ if __name__ == "__main__":
             cat = cat.to_pandas()
 
             for zrange in ('highz', 'lowz', 'midz'):
-
-
                 TIM_params = load_params(f'PAR_FILES/Uchuu_minicubes_for_TIM_{zrange}.par')
 
                 ##Generate the TIM cubes with params precised in TIM_params.par
-                #TIM_params['run_name'] = f"pySIDES_from_uchuu_TIM_tile{l}_{zrange}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz"
-                #file = TIM_params['output_path'] +  TIM_params['run_name'] + '_all_lines_de_Looze_smoothed_MJy_sr.fits' 
-                #if(not os.path.isfile(file)): make_cube(cat, params_sides, TIM_params)
-
-                TIM_params['run_name'] = f"pySIDES_from_uchuu_gaussian_TIM_tile{l}_{zrange}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz"
+                TIM_params['run_name'] = f"pySIDES_from_uchuu_TIM_tile{l}_{zrange}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz"
                 file = TIM_params['output_path'] +  TIM_params['run_name'] + '_full_de_Looze_smoothed_MJy_sr.fits' 
-                if(not os.path.isfile(file)): make_cube(cat, params_sides, TIM_params, filter=True)
+                if(not os.path.isfile(file)): make_cube(cat, params_sides, TIM_params)
+
+                for profile in ('gaussian','lorentzian'):
+                    TIM_params['profile'] = profile
+
+                    TIM_params['run_name'] = f"pySIDES_from_uchuu_{profile}_TIM_tile{l}_{zrange}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz"
+                    file = TIM_params['output_path'] +  TIM_params['run_name'] + '_full_de_Looze_smoothed_MJy_sr.fits' 
+                    if(not os.path.isfile(file)): make_cube(cat, params_sides, TIM_params)
