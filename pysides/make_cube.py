0a76094af6c9684088a559edfc18fcb0b126d8c2
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-02-07 12:41:19 -0600

    update

--- pysides/make_cube.py
+++ pysides/make_cube.py
@@ -273,7 +273,7 @@ def make_continuum_cube(cat, params_sides, params, cube_prop_dict, filter='topha
 
     continuum_nobeam_Jypix = []
 
-    channels_flux_densities = channel_flux_densities(cat, params_sides,cube_prop_dict, profile=filter)
+    channels_flux_densities = channel_flux_densities(cat, params_sides,cube_prop_dict, params,profile=filter)
 
     for f in range(0, cube_prop_dict['shape'][0]):      
         row = channels_flux_densities[:,f] #Jy/pix
