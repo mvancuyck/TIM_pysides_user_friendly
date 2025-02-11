9829bacf90e4f3f99280272b8fab08d051e8425c
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-02-11 13:00:12 -0600

    update

--- gen_all_sizes_TIM_angular_spectral_cubes.py
+++ gen_all_sizes_TIM_angular_spectral_cubes.py
@@ -106,12 +106,18 @@ if __name__ == "__main__":
 
                 #To uncomment later for the gif.
                 #-------------------------------
-                '''
                 TIM_params['profile'] = 'gaussian'
-                TIM_params['run_name'] = f"pySIDES_from_uchuu_gaussian_TIM_tile{l}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz_minus{TIM_params['diff_btw_freq_resol_and_fwhm']/1e9:.1f}GHz_forfwhm"
-                file = TIM_params['output_path'] +  TIM_params['run_name'] + '_full_de_Looze_smoothed_MJy_sr.fits' 
-                if(not os.path.isfile(file) and l==0 and ((tile_sizeRA==0.2) or (tile_sizeRA==1.414))):  make_cube(cat, params_sides, TIM_params)
-                '''
+                embed()
+                freqs = np.arange(TIM_params['freq_min'], TIM_params['freq_max']+TIM_params['freq_resol'], TIM_params['freq_resol'])
+                nbin = 10
+                bins_low = freqs[::nbin]
+                bins_high = freqs[::nbin]+(nbin-1)*TIM_params['freq_resol']
+                for j, (low, high) in enumerate(zip(bins_low, bins_high)):
+                    TIM_params['run_name'] = f"pySIDES_from_uchuu_gaussian_TIM_tile{l}_bin{j}_over_{len(bins_low)}_{tile_sizeRA}deg_{tile_sizeDEC}deg_res{TIM_params['pixel_size']:.0f}arcsec_dnu{TIM_params['freq_resol']/1e9:.1f}GHz_minus{TIM_params['diff_btw_freq_resol_and_fwhm']/1e9:.1f}GHz_forfwhm"
+                    TIM_params['freq_max'] = high
+                    TIM_params['freq_min'] = low
+                    file = TIM_params['output_path'] +  TIM_params['run_name'] + '_full_de_Looze_smoothed_MJy_sr.fits' 
+                    if(not os.path.isfile(file) and l==0 and ((tile_sizeRA==0.2) or (tile_sizeRA==1.414))):  make_cube(cat, params_sides, TIM_params)
                 #-------------------------------
 
                 #Generate the CONCERTO cubes if wanted
