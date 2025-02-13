f95484ffc901e696c3d8c118d86679d7fc00673e
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-02-12 18:07:00 -0600

    set_k.py

--- set_k.py
+++ set_k.py
@@ -13,7 +13,7 @@ from camb import get_matter_power_interpolator
 from camb import model, initialpower
 import pandas as pd
 
-def my_p2(data_map, res, k_bins, unitpk, unitk, map2 = None):
+def my_p2(data_map, res, k_bins, unitpk, unitk, map2 = None, linearbinTrue_logbinFalse = False):
     """
     Measure the power spectrum  in a map
 
@@ -38,17 +38,20 @@ def my_p2(data_map, res, k_bins, unitpk, unitk, map2 = None):
         p2  = np.real(0.5*( (ft*np.conj(ft1)) + (np.conj(ft)*ft1) ))  * norm
     else: p2 = np.abs( (np.fft.fft2( data_map))**2) * norm
     
-    pk_1d, edges = np.histogram(k_map, bins = k_bins, weights = p2)
-    k, edges = np.histogram(k_map, bins = k_bins, weights = k_map)
     histo, edegs = np.histogram(k_map, bins = k_bins)
+    pk_1d, edges = np.histogram(k_map, bins = k_bins, weights = p2)
     pk_1d /= histo
-    k /= histo
+
+    if linearbinTrue_logbinFalse: k = k_bins[:-1] + np.diff(k_bins)/2
+    else: 
+        k, edges = np.histogram(k_map, bins = k_bins, weights = k_map)
+        k /= histo
     
     return pk_1d.to(unitpk), k.to(unitk)
 
 def give_map_spatial_freq(res, ny, nx):
     
-    #res = res.to(u.rad)
+    res = res.to(u.rad)
     Ys, Xs = np.unravel_index(np.arange(nx*ny),(ny,nx))
     N = np.zeros((ny, nx))
     M = np.zeros((ny, nx))
@@ -59,32 +62,18 @@ def give_map_spatial_freq(res, ny, nx):
     M[w_sup] = ny - M[w_sup]
     w_sup = np.where(N>nx/2)
     N[w_sup] = nx - N[w_sup]
-    Kx = M/nx/res
-    Ky = N/ny/res
+    Kx = N/nx/res
+    Ky = M/ny/res
     k_map = (Kx**2 + Ky**2)**(1/2)
         
     return k_map
 
-def give_map_spatial_freq_one_axis(res, ny):
-
-    Ys = np.arange(ny)
-    M = np.zeros(ny)
-    for ys, in zip(Ys):
-        M[ys] = int(ys)
-    w_sup = np.where(M>ny/2)
-    M[w_sup] = ny - M[w_sup]
-    Ky = M/ny/res
-    k_map = (Ky**2)**(1/2)
-    return k_map
-
 def make_bintab(k, delta_k_min, dkk = 0, delta_k_max=0, ):
-
-    k = np.asarray(k)
-    kmax      = k.max()
-    kmin      = k.min()
+    kmax      = k[1].value
+    kmin      = k[0].value
     if(dkk == 0): 
-        bintab = np.arange(kmin, kmax, delta_k_min )
-        bin_width = np.ones(len(bintab)-1) * delta_k_min
+        bintab = np.arange(kmin, kmax, delta_k_min.value )
+        bin_width = np.ones(len(bintab)-1) * delta_k_min.value
     else:
         k1 = kmin
         delta_k = 0 
@@ -92,9 +81,9 @@ def make_bintab(k, delta_k_min, dkk = 0, delta_k_max=0, ):
         bintab.append(kmin)
         bin_width = []
         while(k1 + delta_k <= kmax):
-            delta_k = np.minimum( np.maximum(k1*dkk, delta_k_min) , (kmax - k1) )
+            delta_k = np.minimum( np.maximum(k1*dkk, delta_k_min.value) , (kmax - k1) )
             if(delta_k_max != 0): 
-                delta_k = np.minimum(delta_k, delta_k_max)
+                delta_k = np.minimum(delta_k, delta_k_max.value)
             k1 = k1 +  delta_k
             bintab.append(k1)
             bin_width.append(delta_k)
@@ -103,7 +92,7 @@ def make_bintab(k, delta_k_min, dkk = 0, delta_k_max=0, ):
         bintab[-1]    = kmax
         bin_width[-1] = bintab[-1]-bintab[-2]
     #bintab = np.insert(bintab,0,0)
-    return bintab  , bin_width 
+    return bintab * k[0].unit , bin_width * k[0].unit
     
 def set_k_infos(ny, nx, res, delta_k_over_k = 0):
 
@@ -124,5 +113,5 @@ def set_k_infos(ny, nx, res, delta_k_over_k = 0):
     k_out, edges = np.histogram(k_map, bins = k_bin_tab, weights = k_map)
     histo, edegs = np.histogram(k_map, bins = k_bin_tab)
     k_out = k_out / histo
-    return k_nyquist, k_min, k_bin_width, k_bin_tab, k_out, k_map, histo
+    return k_nyquist, k_min, k_bin_width, k_bin_tab, k_out, k_map
 
